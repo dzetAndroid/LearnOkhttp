078027ed57923a4c01ee58911111266cfd0d6d34
haomengjie <qqduxingzhe@163.com>
2017-06-20 13:01:18 +0800

    little change

--- learnokhttputils/src/main/java/com/zet/learnokhttp3/MainActivity.java
+++ learnokhttputils/src/main/java/com/zet/learnokhttp3/MainActivity.java
@@ -3,6 +3,9 @@ package com.zet.learnokhttp3;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
@@ -12,12 +15,18 @@ import okhttp3.Call;
 /**
  * 主活动Activity
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * 静态常量TAG
      */
     private static final String TAG = "MainActivity";
+
+    private static String baseUrl = "http://192.168.137.1:8080/web/";
+
+    private TextView mTVInfo;
+    private Button mBtnGet_1;
+    private Button mBtnGet_2;
 
     /**
      * 创建
@@ -28,14 +37,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if (savedInstanceState == null) {
+            initView();
+        }
     }
 
-    private static String baseUrl = "http://192.168.137.1:8080/web/";
+    private void initView() {
+        mTVInfo = (TextView) findViewById(R.id.mTVInfo);
+        mBtnGet_1 = (Button) findViewById(R.id.mBtnGet_1);
+        mBtnGet_2 = (Button) findViewById(R.id.mBtnGet_2);
+
+        mBtnGet_1.setOnClickListener(this);
+        mBtnGet_2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mBtnGet_1:
+                getHtml();
+                break;
+            case R.id.mBtnGet_2:
+                getGitHubHtml();
+                break;
+        }
+    }
 
     private void getHtml() {
-        String url = baseUrl + "login";
-
+        String url = baseUrl + "User_login";
         OkHttpUtils
                 .get()
                 .url(url)
@@ -54,4 +83,25 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void getGitHubHtml() {
+        String url = "http://www.piaotian.com/";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e(TAG, "onResponse: " + response);
+                        mTVInfo.setText(response);
+                    }
+                });
+    }
+
 }
